c6f43779de408062c9ce88f9dbd127c7f6c79371
Syed Hamadan Ahmad <f20212337@goa.bits-pilani.ac.in>
2023-11-09 04:42:28 +0530

    Model Analysis done

--- model.py
+++ model.py
@@ -1,10 +1,17 @@
+import random
+import string
+import numpy as np
+from keras.utils import pad_sequences
+from keras.preprocessing.text import Tokenizer
+tokenizer=Tokenizer()
+from data import responses
 from preprocess_data import input_shape, total_words, output_length, x_train, y_train
 from keras.models import Model
 from keras.layers import Input, Embedding, Dense, Flatten, LSTM
 
 i=Input(shape=(input_shape, ))
-x=Embedding(total_words+1, 30,)(i)
-x=LSTM(30, return_sequences=True)(x)
+x=Embedding(total_words+1, 10)(i)
+x=LSTM(10, return_sequences=True)(x)
 x=Flatten()(x)
 x=Dense(output_length, activation='softmax')(x)
 
@@ -13,3 +20,7 @@ model=Model(i,x)
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 train=model.fit(x_train, y_train, epochs=200)
+model.save("LSTM Model", train)
+
+
+
